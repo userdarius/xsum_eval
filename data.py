aff00de873007c220c7a74850ef36fe36f351c66
Darius Foodeei <darius.foodei@gmail.com>
2025-01-04 19:04:02 +0100

    trust remote code for dataset loading

--- data.py
+++ data.py
@@ -7,5 +7,5 @@ def get_dataset(name):
     """Load and return a dataset by name."""
     print(f"Loading {name} dataset")
 
-    df = datasets.load_dataset(name)
+    df = datasets.load_dataset(name, trust_remote_code=True)
     return df
